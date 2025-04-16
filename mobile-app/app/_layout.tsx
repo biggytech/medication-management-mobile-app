37e3833bfdf6cb6db25d8763d1bc4df867bfb300
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-04-16 23:19:44 +0300

    feat(mobile): basic app layout with auth provider

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,10 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+// import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import {AuthProvider} from "@/providers/AuthProvider";
+import {Slot} from "expo-router";
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -29,11 +31,10 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <AuthProvider>
+        <Slot />
+      </AuthProvider>
+      {/*<StatusBar style="auto" />*/}
     </ThemeProvider>
   );
 }
