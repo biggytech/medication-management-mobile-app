48388325c92a83b66fd82cb112a6a3a938c1d18e
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-05-01 17:13:21 +0300

    feat(mobile): create Input component

---
+++ mobile-app/assets/colors.ts
@@ -0,0 +1,7 @@
+export enum AppColors {
+  POSITIVE = "#C2E812",
+  NEGATIVE = "#FF934F",
+  PRIMARY = "#91F5AD",
+  SECONDARY = "#BFCBC2",
+  BACKGROUND = "#FFFFFF",
+}
---
+++ mobile-app/components/Input.tsx
@@ -0,0 +1,20 @@
+import type React from "react";
+import { TextInput, StyleSheet, type TextInputProps } from "react-native";
+import { AppColors } from "@/assets/colors";
+
+type InputProps = TextInputProps;
+
+export const Input: React.FC<InputProps> = ({ style, ...props }) => {
+  return <TextInput style={[styles.input, style]} {...props} />;
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: 200,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: AppColors.SECONDARY,
+    padding: 10,
+  },
+});
