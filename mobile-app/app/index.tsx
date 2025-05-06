8ba1edd5376011337078142feacdb5ed270e0a9e
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-05-06 22:47:07 +0300

    feat(mobile): load selected language before starting the app

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -1,12 +1,22 @@
 import { useAuthSession } from "@/providers/AuthProvider";
 import { Redirect } from "expo-router";
-import type { ReactNode } from "react";
+import { type ReactNode, useEffect, useState } from "react";
 import { Loader } from "@/components/Loader";
+import { LanguageService } from "@/services/language/LanguageService";
 
 export default function RootLayout(): ReactNode {
-  const { getToken, isLoading } = useAuthSession();
+  const { getToken, isLoading: isTokenLoading } = useAuthSession();
+  const [isLanguageLoading, setIsLanguageLoading] = useState<boolean>(true);
 
-  if (isLoading) {
+  useEffect(() => {
+    (async () => {
+      // load selected language before starting the app
+      await LanguageService.loadCurrentLanguage();
+      setIsLanguageLoading(false);
+    })();
+  }, []);
+
+  if (isTokenLoading || isLanguageLoading) {
     return <Loader />;
   }
 
