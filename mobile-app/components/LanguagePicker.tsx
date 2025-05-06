e3394a34c2b7f8933152e2333ab5860c76e39c62
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-05-06 22:57:00 +0300

    feat(mobile): reload screen when language changed

--- mobile-app/components/LanguagePicker.tsx
+++ mobile-app/components/LanguagePicker.tsx
@@ -1,98 +1,104 @@
-import {useState, useEffect} from "react";
-import { View, StyleSheet} from "react-native";
-import { Dropdown } from 'react-native-element-dropdown';
-import {LanguageService} from "@/services/language/LanguageService";
+import { useState, useEffect } from "react";
+import { View, StyleSheet } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import { LanguageService } from "@/services/language/LanguageService";
+import { useScreenReload } from "@/hooks/useScreenReload";
 
 interface LanguageOption {
-    label: string;
-    value: string;
+  label: string;
+  value: string;
 }
 
 const LANGUAGES_OPTIONS: LanguageOption[] = [
-    {
-        label: '🇺🇸  English',
-        value: 'en-US'
-    },
-    {
-        label: '🇷🇺  Русский',
-        value: 'ru-RU'
-    }
-]
+  {
+    label: "🇺🇸  English",
+    value: "en-US",
+  },
+  {
+    label: "🇷🇺  Русский",
+    value: "ru-RU",
+  },
+];
 
 export const LanguagePicker = () => {
-    const [selectedLanguage, setSelectedLanguage] = useState<string | null>(LanguageService.DEFAULT_LANGUAGE);
-    const [isFocus, setIsFocus] = useState<boolean>(false);
+  const [selectedLanguage, setSelectedLanguage] = useState<string | null>(
+    LanguageService.DEFAULT_LANGUAGE,
+  );
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+  const { reloadScreen } = useScreenReload();
 
-    useEffect(() => {
-        (async () => {
-            const language = await LanguageService.loadCurrentLanguage();
-            if (language) {
-                setSelectedLanguage(language)
-            } else {
-                await LanguageService.changeLanguage(LanguageService.DEFAULT_LANGUAGE);
-            }
-        })()
-    }, []);
-
-    const handleLanguageUpdated = async (option: LanguageOption) => {
-        const language = option.value;
-
+  useEffect(() => {
+    (async () => {
+      const language = await LanguageService.loadCurrentLanguage();
+      if (language) {
         setSelectedLanguage(language);
-        setIsFocus(false)
+      } else {
+        await LanguageService.changeLanguage(LanguageService.DEFAULT_LANGUAGE);
+      }
+    })();
+  }, []);
 
-        await LanguageService.changeLanguage(language)
-    }
+  const handleLanguageUpdated = async (option: LanguageOption) => {
+    const language = option.value;
 
-    return <View>
-        {selectedLanguage && <Dropdown
-            style={[styles.dropdown, isFocus && styles.focusedDropdown]}
-            placeholderStyle={styles.placeholderStyle}
-            selectedTextStyle={styles.selectedTextStyle}
-            containerStyle={styles.dropdownContainer}
-            iconStyle={styles.iconStyle}
-            itemContainerStyle={styles.itemContainerStyle}
-            itemTextStyle={styles.itemTextStyle}
-            data={LANGUAGES_OPTIONS}
-            maxHeight={300}
-            labelField="label"
-            valueField="value"
-            placeholder={!isFocus ? LanguageService.translate('Select') : '...'}
-            value={selectedLanguage}
-            onFocus={() => setIsFocus(true)}
-            onBlur={() => setIsFocus(false)}
-            onChange={handleLanguageUpdated}
-        />}
+    setSelectedLanguage(language);
+    setIsFocus(false);
+
+    await LanguageService.changeLanguage(language);
+    reloadScreen();
+  };
+
+  return (
+    <View>
+      {selectedLanguage && (
+        <Dropdown
+          style={[styles.dropdown, isFocus && styles.focusedDropdown]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          containerStyle={styles.dropdownContainer}
+          iconStyle={styles.iconStyle}
+          itemContainerStyle={styles.itemContainerStyle}
+          itemTextStyle={styles.itemTextStyle}
+          data={LANGUAGES_OPTIONS}
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocus ? LanguageService.translate("Select") : "..."}
+          value={selectedLanguage}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={handleLanguageUpdated}
+        />
+      )}
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    dropdownContainer: {
-
-    },
-    dropdown: {
-        height: 50,
-        borderColor: 'gray',
-        borderWidth: 0.5,
-        borderRadius: 8,
-        paddingHorizontal: 8,
-        width: 120,
-    },
-    focusedDropdown: {
-        borderColor: 'blue'
-    },
-    placeholderStyle: {
-        fontSize: 14,
-    },
-    selectedTextStyle: {
-        fontSize: 14,
-    },
-    iconStyle: {
-        width: 20,
-        height: 20,
-    },
-    itemContainerStyle: {
-    },
-    itemTextStyle: {
-        fontSize: 14,
-    }
+  dropdownContainer: {},
+  dropdown: {
+    height: 50,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    width: 120,
+  },
+  focusedDropdown: {
+    borderColor: "blue",
+  },
+  placeholderStyle: {
+    fontSize: 14,
+  },
+  selectedTextStyle: {
+    fontSize: 14,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  itemContainerStyle: {},
+  itemTextStyle: {
+    fontSize: 14,
+  },
 });
---
+++ mobile-app/hooks/useScreenReload.ts
@@ -0,0 +1,17 @@
+import { useRouter, usePathname } from "expo-router";
+import { useCallback } from "react";
+
+/**
+ * Allows reloading of current screen
+ */
+export const useScreenReload = () => {
+  const router = useRouter();
+  const routePath = usePathname();
+
+  const reloadScreen = useCallback(() => {
+    // @ts-expect-error - routePath is dynamic
+    router.replace(routePath);
+  }, [routePath]);
+
+  return { reloadScreen };
+};
